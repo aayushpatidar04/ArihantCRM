29b6fb51425c0147bf15bd8b36f69bd8df874c3c
Aayush Patidar <aayushpatidar04@gmail.com>
2026-09-23 16:24:33 +0530

    agenr primary department log

--- app/Console/Commands/BitrixSyncCommand.php
+++ app/Console/Commands/BitrixSyncCommand.php
@@ -1068,13 +1068,9 @@ class BitrixSyncCommand extends Command
          *      =
          * exactly ONE record for the executive
          */
-
+        \Log::info($this->agentPrimaryDept);
         foreach ($this->agentPrimaryDept as $bitrixUserId => $departmentId) {
 
-            if (in_array($bitrixUserId, ['5155', '5157', '5159'])) {
-                \Log::info('Bitrix User Id: ' . $bitrixUserId);
-                \Log::info($this->userMap[$bitrixUserId] ?? 'No bitrix user found');
-            }
             if (!isset($this->userMap[$bitrixUserId])) {
                 continue;
             }
